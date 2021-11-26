8326040406832a24249ab0691842fc75114b0e93
YesCoops <coopandmgc@gmail.com>
2021-11-26 11:35:43 +0000

    Created questions.js and inputted questions and answers

---
+++ assets/js/questions.js
@@ -0,0 +1,169 @@
+const questions = [
+    {
+        question = 'When newborn babies cry, they produce tears. True or False?',
+        answers = [
+            {text: 'True', correct: false},
+            {text: 'False', correct: true}
+        ]
+    },
+
+    {
+        question = 'What percentage of babies actually arrive on their due date?',
+        answers = [
+            {text: 'Less than 2%', correct: false},
+            {text: '3-4%', correct: true},
+            {text: '10%', correct: false},
+            {text: '25%', correct: false}
+        ]
+    },
+
+    {
+        question = 'Babies are born without which body part?',
+        answers = [
+            {text: 'Kneecaps', correct: true},
+            {text: 'Elbows', correct: false},
+            {text: 'Tailbone', correct: false},
+            {text: 'Shoulder Blades', correct: false}
+
+        ]
+    },
+
+    {
+        question = 'How many bones is a baby born with?',
+        answers = [
+            {text: '200', correct: false},
+            {text: '206', correct: false},
+            {text: '250', correct: false},
+            {text: '300', correct: true}
+        ]
+    },
+
+    {
+        question = 'How many nappies does the average baby go through in one year?',
+        answers = [
+            {text: '250', correct: false},
+            {text: '2500', correct: false},
+            {text: '5000', correct: false},
+            {text: '7500', correct: true}
+        ]
+    },
+
+    {
+        question = 'How many people does the average child share his/her birthday with?',
+        answers = [
+            {text: '2 Billion', correct: false},
+            {text: '225 Million', correct: false},
+            {text: '9 Million', correct: true},
+            {text: '1 Million', correct: false}
+        ]
+    },
+
+    {
+        question = 'All babies are born with blue eyes. True or False?',
+        answers = [
+            {text: 'True', correct: false},
+            {text: 'False', correct: true}
+        ]
+    },
+
+    {
+        question = "When born, what size is a baby's head in relation to the rest of his/her body?",
+        answers = [
+            {text: 'One eighth the size', correct: false},
+            {text: 'One quarter the size', correct: true},
+            {text: 'Half the size', correct: false},
+            {text: 'Twice the size', correct: false}
+        ]
+    },
+
+    {
+        question = "What colour was used for boys in the 1900's?",
+        answers = [
+            {text: 'Pink', correct: true},
+            {text: 'Yellow', correct: false},
+            {text: 'Blue', correct: false},
+            {text: 'Green', correct: false}
+        ]
+
+    },
+
+    {
+        question = 'How often is a baby born around the world?',
+        answers = [
+            {text: 'Every minute', correct: false},
+            {text: 'Every thirty seconds', correct: false},
+            {text: 'Every three seconds', correct: true},
+            {text: 'Every second', correct: false}
+        ]
+    },
+
+    {
+        question = "Babies recognize their mother's voice at birth; how long before they recognize their father's?",
+        answers = [
+            {text: 'One week', correct: false},
+            {text: 'Two weeks', correct: true},
+            {text: 'One month', correct: false},
+            {text: 'Never', correct: false}
+        ]
+    },
+
+    {
+        question = "A baby's head accounts for how much of his/her body weight?",
+        answers = [
+            {text: '20%', correct: true},
+            {text: '50%', correct: false},
+            {text: '70%', correct: false},
+            {text: '90%', correct: false}
+        ]
+    },
+
+    {
+        question = "When does a baby get his/her fingerprints?",
+        answers = [
+            {text: 'As a 3 month foetus', correct: true},
+            {text: 'As a 6 month foetus', correct: false},
+            {text: 'Shortly after birth', correct: false},
+            {text: '6 months after birth', correct: false}
+        ]
+    },
+
+    {
+        question = 'What can a baby do up until seven months that an adult cannot?',
+        answers = [
+            {text: 'Cross their eyes whilst sticking out their tongue', correct: false},
+            {text: 'Lick their elbow', correct: false},
+            {text: 'Breathe and swallow simultaneously', correct: true},
+            {text: 'Sneeze with their eyes open', correct: false}
+        ]
+    },
+
+    {
+        question = 'Newborns smile in their sleep because...',
+        answers = [
+            {text: 'They have gas', correct: false},
+            {text: 'Their brains are developing', correct: true},
+            {text: 'They are dreaming', correct: false},
+            {text: 'They heard a funny joke', correct: false}
+        ]
+    },
+
+    {
+        question = "A newborn babys stomach is the size of a...",
+        answers = [
+            {text: 'A marble', correct: true},
+            {text: 'A ping pong ball', correct: false},
+            {text: 'An egg', correct: false},
+            {text: 'An orange', correct: false}
+        ]
+    },
+
+    {
+        question = 'By the end of the fifth month, most babies know...',
+        answers = [
+            {text: 'How to read', correct: false},
+            {text: 'How to chew and swallow baby food', correct: false},
+            {text: 'Their name', correct: true},
+            {text: 'Their ABCs', correct: false}
+        ]
+    },
+];
