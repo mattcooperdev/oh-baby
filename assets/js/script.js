bb8f24647594af292319c3b10e3bd8cd962aefd3
YesCoops <coopandmgc@gmail.com>
2021-11-29 15:55:46 +0000

    Updated styling of rules modal and created active classes to hide rules and allow Game modal to show

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,13 @@ function incrementWrongAnswer() {
 
 }
 
+let startBtn = document.querySelector('.start-btn')
+let modalBg = document.querySelector('.modal-bg')
+let modalClose = document.querySelector('.modal-close')
+
+startBtn.addEventListener('click', function(){
+    modalBg.classlist.add('.game-area-active');
+})
+modalClose.addEventListener('click', function(){
+    modalClose.classList.remove('.game-area-active')
+})
