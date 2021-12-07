4a0c33d4d3bdfd83e94961bc33c0133eaaacdf5a
YesCoops <coopandmgc@gmail.com>
2021-12-07 11:41:50 +0000

    Moved name input into Main Area and created variable to allow adding and removing of hide class when required

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,7 @@ function incrementWrongAnswer() {
 
 let startBtn = document.querySelector('.start-btn')
 let rulesBtn = document.querySelector('.rules-btn')
+let nameBox = document.querySelector('.name-input')
 let mainArea = document.querySelector('.main-area')
 let rulesArea = document.querySelector('.rules-area')
 let gameArea = document.querySelector('.game-area')
@@ -30,15 +31,21 @@ let nextBtn = document.querySelector('next-btn')
 startBtn.addEventListener('click', () => {
     runGame()
     gameArea.classList.remove("hide");
-    mainArea.classList.add("hide");
+    mainArea.classList.add("hide")
 })
 
 rulesBtn.addEventListener('click', () => {
     rulesArea.classList.remove("hide");
     mainArea.classList.add("modal-bg")
+    startBtn.classList.add("hide");
+    rulesBtn.classList.add("hide");
+    nameBox.classList.add("hide")
 })
 
 modalClose.addEventListener('click', () => {
     rulesArea.classList.add("hide");
-    mainArea.classList.remove("modal-bg")
+    mainArea.classList.remove("modal-bg");
+    startBtn.classList.remove("hide");
+    rulesBtn.classList.remove("hide");
+    nameBox.classList.remove("hide")
 })
